ab154446c3a6e15675780e32f143be1f004bc835
Aryog <yogesh000aryal@gmail.com>
2024-11-29 20:26:07 +0545

    fix: rate limit issue resolved

---
+++ src/lib/rateLimit/RateLimitedAgent.ts
@@ -0,0 +1,73 @@
+import AtpAgent from "@atproto/api"
+export class RateLimitedAgent {
+  private agent: AtpAgent;
+  private waitingForRateLimit: boolean = false;
+
+  constructor(agent: AtpAgent) {
+    this.agent = agent;
+  }
+
+  private async handleRateLimit(error: any): Promise<void> {
+    if (error.status === 429) {
+      this.waitingForRateLimit = true;
+      const resetTime = new Date(Number(error.headers['ratelimit-reset']) * 1000);
+      const waitTime = resetTime.getTime() - Date.now();
+      console.log(`Rate limit exceeded. Waiting until ${resetTime.toLocaleString()} (${Math.ceil(waitTime / 1000)} seconds)`);
+      await new Promise(resolve => setTimeout(resolve, waitTime));
+      this.waitingForRateLimit = false;
+    } else {
+      throw error;
+    }
+  }
+
+  async call<T>(method: () => Promise<T>): Promise<T> {
+    let attempts = 0;
+    while (true) {
+      try {
+        if (this.waitingForRateLimit) {
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          continue;
+        }
+        return await method();
+      } catch (error: any) {
+        if (++attempts > 5) {
+          throw error;
+        }
+        if (error.message.includes('fetch failed')) {
+          console.warn(`Fetch failed, retrying attempt ${attempts}/5...`);
+          await new Promise(resolve => setTimeout(resolve, 2000));
+          continue;
+        }
+        if (error.status === 429) {
+          await this.handleRateLimit(error);
+        } else {
+          throw error;
+        }
+      }
+    }
+  }
+
+  async uploadBlob(...args: Parameters<typeof AtpAgent.prototype.uploadBlob>) {
+    return this.call(() => this.agent.uploadBlob(...args));
+  }
+
+  async post(...args: Parameters<typeof AtpAgent.prototype.post>) {
+    return this.call(() => this.agent.post(...args));
+  }
+
+  async login(...args: Parameters<typeof AtpAgent.prototype.login>) {
+    return this.call(() => this.agent.login(...args));
+  }
+
+  async getServiceAuth(...args: Parameters<typeof AtpAgent.prototype.com.atproto.server.getServiceAuth>) {
+    return this.call(() => this.agent.com.atproto.server.getServiceAuth(...args));
+  }
+
+  get session() {
+    return this.agent.session;
+  }
+
+  get dispatchUrl() {
+    return this.agent.dispatchUrl;
+  }
+}
