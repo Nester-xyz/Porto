e489419cfc7ca8dcc18c9ddc950de0c93ac56340
Ankit Bhandari <85058931+whoisanku@users.noreply.github.com>
2025-06-06 17:21:06 +0545

    feat: add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Login from "./components/pages/login/Login";
 import { LogInProvider } from "./hooks/LogInContext";
+import ThemeToggle from "./components/ThemeToggle";
 
 const App: React.FC = () => {
   return (
     <LogInProvider>
+      <div className="absolute top-4 right-4">
+        <ThemeToggle />
+      </div>
       <Login />
     </LogInProvider>
   );
